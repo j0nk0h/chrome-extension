916031765d70a9cbd13c66a9bdee3ca36b069d3d
j0nk0h <jonkoh39@gmail.com>
2022-04-24 22:26:04 -0700

    fixed image text bug

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ contents.remove();
 const newImgNode = $('<img id="the-img" src="https://upload.wikimedia.org/wikipedia/en/thumb/f/ff/SuccessKid.jpg/256px-SuccessKid.jpg">');
 console.log(newImgNode);
 contentsParent.prepend(newImgNode);
-const dreamText = $(<p>Get back to achieving your coding dreams</p>)
+const dreamText = $("<p>Get back to achieving your coding dreams</p>")
 newImgNode.prepend(dreamText);
